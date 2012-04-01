ad8f24fb1d738d50611cd608b18e9fa4f2a4bc97
Patric Fornasier <patric.fornasier@gmail.com>
2012-04-01 23:07:31 +0100

    pat: reverse geo lookup and using real twitter data.

--- web.rb
+++ web.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'twitter'
 
+Twitter.configure do |config|                        
+  config.endpoint = 'http://' + ENV['APIGEE_TWITTER_API_ENDPOINT']     
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+end
+
 get '/' do
-  #@tweet = Twitter.user_timeline("patforna").first.text
-  @tweet = "We're in the corner."
-  #Twitter.reverse_geocode(:lat => "37.7821120598956", :long => "-122.400612831116")
-  
+  @tweet = Twitter.user_timeline("patforna").first  
+  @location = Twitter.reverse_geocode(:lat => @tweet.geo.latitude, :long => @tweet.geo.longitude, :max_results => 1).first
   erb :index
 end
