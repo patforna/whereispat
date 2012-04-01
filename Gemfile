acd397b16e698b846a1e34fa1e9dbd0e039544ea
Patric Fornasier <patric.fornasier@gmail.com>
2012-04-01 21:14:42 +0100

    pat: started integrating twitter api.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 gem 'sinatra', '1.3.2'
 gem 'thin', '1.3.1'
+gem "twitter", "~> 2.2.0"
--- web.rb
+++ web.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'twitter'
 
 get '/' do
-  "Still in the UK."
+  #@tweet = Twitter.user_timeline("patforna").first.text
+  @tweet = "We're in the corner."
+  #Twitter.reverse_geocode(:lat => "37.7821120598956", :long => "-122.400612831116")
+  
+  erb :index
 end
