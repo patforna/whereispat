717be145d53828b914fcdcf3351025291c3fe9b7
Tom Duckering <tom.duckering@gmail.com>
2012-04-14 01:54:29 +0100

    Tom - adding a graph of speed over time

--- web.rb
+++ web.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 require 'twitter'
+require 'geokit'
+
+Geokit::default_units = :kms
+Geokit::default_formula = :sphere
 
 average_cycling_speed_mph = 5
 start_date = Time.new(2012,4,3)
@@ -42,16 +46,41 @@ get '/' do
       @past_locations.push({:lat => lat, :long => long, :time => tweet.created_at, :text => tweet.text })
     end
   }
+  
+  @past_locations.reverse!
+  
+  previous_location = @past_locations[0];
+  first_location = @past_locations[0];
+  first_LL = Geokit::LatLng.new(first_location[:lat],first_location[:long])
+  
+  @distance_time_speeds = @past_locations.drop(1).map {|location|
+    previous_LL = Geokit::LatLng.new(previous_location[:lat],previous_location[:long])
+    current_LL = Geokit::LatLng.new(location[:lat],location[:long])
+    distance = previous_LL.distance_to(current_LL)
+    culmulative_distance = first_LL.distance_to(current_LL)
+    
+    time_between_locations = ((location[:time] - previous_location[:time]) / 3600).round(2)
+    culmulative_time = ((location[:time] - first_location[:time]) / 3600).round(2)
+    
+    speed = (distance / time_between_locations.to_f).round(2)
+    
+    
+    
+    previous_location = location
+    @total_distance = culmulative_distance
+    @total_time = culmulative_time
+    {:distance_in_kilometers => distance,:culmulative_distance_in_kilometers => culmulative_distance, :culmulative_time_in_hours => culmulative_time, :time_diff_in_hours => time_between_locations, :speed_in_kph => speed}
+  }
 
   @hours_since_last_tweet = ((Time.new() - @last_tweet.created_at) / 3600).round
   @how_far_might_he_have_gone = @hours_since_last_tweet * average_cycling_speed_mph
 
-  @lookup_lat = @past_locations[0][:lat]
-  @lookup_long = @past_locations[0][:long]
-  @time_at_location = @past_locations[0][:time]
+  @lookup_lat = @past_locations.last[:lat]
+  @lookup_long = @past_locations.last[:long]
+  @time_at_location = @past_locations.last[:time]
 
   begin
-    @last_location = Twitter.reverse_geocode(:lat => @past_locations[0][:lat], :long => @past_locations[0][:long], :max_results => 1, :granularity => "city").first.full_name
+    @last_location = Twitter.reverse_geocode(:lat => @lookup_lat, :long => @lookup_long, :max_results => 1, :granularity => "city").first.full_name
   rescue
     @last_location = "Not sure the name of the place"
   end
