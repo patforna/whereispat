09137338b06be0b82d0b2fb7f538649f57145923
Patric Fornasier <patric.fornasier@gmail.com>
2012-04-19 15:59:00 +0200

    pat: adding functional test infra.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
+# unit tests
 require 'place'
+
+# functional tests
+ENV['RACK_ENV'] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'web.rb')
+require 'rack/test'
+require 'webmock/rspec'
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+  conf.include WebMock::API
+end
---
+++ spec/web_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'The App' do
+  
+  def app
+    Sinatra::Application
+  end
+  
+  before(:each) do
+    stub_request(:get, /.*user_timeline.json.*/).to_return(load('user_timeline.json'))
+    stub_request(:get, /.*reverse_geocode.json.*/).to_return(load('reverse_geocode.json'))
+  end  
+
+  it "should show the last tweet" do
+    get '/'    
+    last_response.body.should include '@daniel_otte I also miss you guys. Hope things are ok.'    
+  end
+  
+  it "should show the last known location" do
+    get '/'
+    last_response.body.should include 'La Spezia, La Spezia'    
+  end
+  
+  private
+  def load(file)
+    File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'curl', file))
+  end
+    
+  
+end
