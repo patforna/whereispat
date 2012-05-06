2bdda88b62b674d38eee3fe2172ba014ad8d645f
Patric Fornasier <patric.fornasier@gmail.com>
2012-05-06 09:06:44 +0300

    pat: using relative time.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'thin', '1.3.1'
 gem 'twitter', '~> 2.2.0'
 gem 'geokit'
 gem 'json'
+gem "time-ago-in-words", "~> 0.1.2"
 
 group :test do
   gem 'rspec'
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,5 +1,11 @@
 module Helpers
+  require 'time-ago-in-words'
+  
   def auto_link(string)
     string.gsub /((https?:\/\/|www\.)([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/, %Q{<a href="\\1">\\1</a>}
+  end
+  
+  def ago_in_words(time)
+     time.ago_in_words
   end
 end
