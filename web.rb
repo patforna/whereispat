2a37c44f57532916aa7fc70046465ec62b8bff12
Patric Fornasier <patric.fornasier@gmail.com>
2012-06-23 08:17:47 +0430

    pat: ditched speed graph because messages are not real time and thus don't permit calculation of speed.

--- web.rb
+++ web.rb
@@ -38,38 +38,6 @@ get '/' do
   @last_tweet = tweets.first
   @route = Route.from(tweets)
   @last_place = @route.last_place
-  
-  
-  
-  @past_locations = Array.new()
-  tweets.each {|tweet|
-    place = Place.parse(tweet)   
-    if !place.unknown?
-      @past_locations.push({:lat => place.latitude, :long => place.longitude, :time => tweet.created_at, :text => tweet.text })
-    end
-  }
-  @past_locations.reverse!
-  
-  previous_location = @past_locations[0];
-  first_location = @past_locations[0];
-  first_LL = Geokit::LatLng.new(first_location[:lat],first_location[:long])
-  
-  @distance_time_speeds = @past_locations.drop(1).map {|location|
-    previous_LL = Geokit::LatLng.new(previous_location[:lat],previous_location[:long])
-    current_LL = Geokit::LatLng.new(location[:lat],location[:long])
-    distance = previous_LL.distance_to(current_LL)
-    culmulative_distance = first_LL.distance_to(current_LL)
-    
-    time_between_locations = ((location[:time] - previous_location[:time]) / 3600).round(2)
-    culmulative_time = ((location[:time] - first_location[:time]) / 3600).round(2)
-    
-    speed = (distance / time_between_locations.to_f).round(2)
-    
-    previous_location = location
-    @total_distance = culmulative_distance
-    @total_time = culmulative_time
-    {:distance_in_kilometers => distance,:culmulative_distance_in_kilometers => culmulative_distance, :culmulative_time_in_hours => culmulative_time, :time_diff_in_hours => time_between_locations, :speed_in_kph => speed}
-  }
 
   @hours_since_last_place = ((Time.new() - @last_place.visited_at) / 3600).round
   @how_far_might_he_have_gone = @hours_since_last_place * average_cycling_speed_mph
