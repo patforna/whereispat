369f6ffb3c8150238e655f9b6cced2be987a3f1d
jennifersmith <jensmith@thoughtworks.com>
2012-07-29 10:43:38 +0200

    Update lib/tweet.rb
    Lazy initiliase tweets class var incase it never gets initialised. Could probably also initialise to an empty array during class deff too!

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -4,6 +4,7 @@ class Tweet
   START_DATE = Time.new(2012,4,10)
   
   def self.load() 
+    @@tweets ||= []
     begin
       @@tweets = Twitter.user_timeline("patforna", :count => 200, :exclude_replies => true, :include_rts => false, :trim_user => true, :include_entities => false).select { |t| t.created_at > START_DATE }
     rescue => e
